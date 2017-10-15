72c8b83c2f73c45ce5fa3b21723ade856b114565
Mikko Vuorinen <vuorinem@gmail.com>
2017-10-15 23:47:18 +0100

    Create view model for navmenu

---
+++ web/ClientApp/app/components/navmenu/navmenu.ts
@@ -0,0 +1,10 @@
+import { autoinject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+@autoinject
+export class Navmenu {
+
+    constructor(private router: Router) {
+    }
+
+}
