1170ae5dfc14319f3dce9cac5e4e393c5785ca32
Mikko Vuorinen <vuorinem@gmail.com>
2017-10-16 00:10:51 +0100

    Add login buttons to navmenu

--- web/ClientApp/app/components/navmenu/navmenu.ts
+++ web/ClientApp/app/components/navmenu/navmenu.ts
@@ -1,10 +1,13 @@
 import { autoinject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
+import { AuthService } from '../auth/auth-service';
 
 @autoinject
 export class Navmenu {
 
-    constructor(private router: Router) {
+    constructor(
+        private router: Router,
+        private authService: AuthService) {
     }
 
 }
